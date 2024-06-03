b75047bafeea0f8385b3591c093374afb06b6ba5
Adam Iwachów <adam.iwachow@gmail.com>
2024-06-03 09:11:22 +0200

    Osobny skrypt do łączenia z bazą

---
+++ Strona/DataBase.php
@@ -0,0 +1,20 @@
+<?php
+$servername = "localhost";  
+$username = "AdamJustynaRezerwacje";
+$password = "Pwr1234BazyDanych";
+$dbname = "rezerwacjaObiektow";
+
+if (!function_exists('mysqli_init') && !extension_loaded('mysqli')) {
+    echo 'Inizjalizacja bazy nieudana';
+} else {
+    echo '';
+}
+
+// Tworzenie połączenia
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+return $conn; 
